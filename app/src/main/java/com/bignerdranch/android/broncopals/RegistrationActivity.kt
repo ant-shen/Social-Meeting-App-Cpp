02b416fdc0e4b0900468125a296f8a5423dd2986
Ericcyyyy <117465025+Ericcyyyy@users.noreply.github.com>
2023-11-29 11:51:58 -0800

    updated and implemented activity_main.xml/activity_registration.xml/RegistrationActivity.kt and updated activity_main.xml

--- app/src/main/java/com/bignerdranch/android/broncopals/RegistrationActivity.kt
+++ app/src/main/java/com/bignerdranch/android/broncopals/RegistrationActivity.kt
@@ -1,11 +1,65 @@
 package com.bignerdranch.android.broncopals
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.bignerdranch.android.broncopals.databinding.ActivityRegistrationBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class RegistrationActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegistrationBinding
+    private lateinit var firebaseDatabase: FirebaseDatabase
+    private lateinit var databaseReference: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registration)
+        binding = ActivityRegistrationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseDatabase = FirebaseDatabase.getInstance()
+        databaseReference = firebaseDatabase.reference.child("users")
+
+        binding.signUpButton.setOnClickListener {
+            val registerUsername = binding.registerUsername.text.toString()
+            val registerPassword = binding.registerPassword.text.toString()
+
+            if (registerUsername.isNotEmpty() && registerPassword.isNotEmpty()){
+                registerUser(registerUsername, registerPassword)
+            } else {
+                Toast.makeText(this@RegistrationActivity, "ALL FIELDS MUST BE FILLED OUT!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.loginRedirect.setOnClickListener {
+            startActivity(Intent(this@RegistrationActivity, LoginActivity::class.java))
+            finish()
+        }
+    }
+
+    private fun registerUser(username: String, password: String){
+        databaseReference.orderByChild("username").equalTo(username).addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (!dataSnapshot.exists()){
+                    val id = databaseReference.push().key
+                    val userData = UserData(id, username, password)
+                    databaseReference.child(id!!).setValue(userData)
+                    Toast.makeText(this@RegistrationActivity, "Sign Up Successful!", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this@RegistrationActivity, LoginActivity::class.java))
+                    finish()
+                } else {
+                    Toast.makeText(this@RegistrationActivity, "User Already Exists!", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Toast.makeText(this@RegistrationActivity, "Database Error: ${databaseError.message}", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
