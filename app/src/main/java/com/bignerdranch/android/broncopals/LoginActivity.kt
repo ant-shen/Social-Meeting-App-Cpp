99edaf838f8f53ab3778ed918962c51cf5181ba2
rosaluW <113726961+rosaluW@users.noreply.github.com>
2023-11-29 23:14:30 -0800

    added login page

--- app/src/main/java/com/bignerdranch/android/broncopals/LoginActivity.kt
+++ app/src/main/java/com/bignerdranch/android/broncopals/LoginActivity.kt
@@ -1,11 +1,45 @@
+//package com.bignerdranch.android.broncopals
+//
+//import androidx.appcompat.app.AppCompatActivity
+//import android.os.Bundle
+//
+//class LoginActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_login)
+//    }
+//}
+
 package com.bignerdranch.android.broncopals
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.bignerdranch.android.broncopals.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    lateinit var username : EditText
+    lateinit var password: EditText
+    lateinit var loginButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener(View.OnClickListener {
+            if (binding.username.text.toString() == "user" && binding.password.text.toString() == "1234"){
+                Toast.makeText(this, "Login Successful!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Login Failed!", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
