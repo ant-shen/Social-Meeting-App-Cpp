6f2be629485ee34ad0b49df394225b62e9f8a69e
ant-shen <anthony.shen11@gmail.com>
2023-12-05 16:59:00 -0800

    test

--- app/src/main/java/com/bignerdranch/android/broncopals/LoginActivity.kt
+++ app/src/main/java/com/bignerdranch/android/broncopals/LoginActivity.kt
@@ -1,15 +1,16 @@
 package com.bignerdranch.android.broncopals
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.bignerdranch.android.broncopals.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.auth.FirebaseAuth
-
-
+import com.google.firebase.database.ValueEventListener
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
@@ -25,27 +26,22 @@ class LoginActivity : AppCompatActivity() {
         firebaseAuth = FirebaseAuth.getInstance()
         databaseReference = FirebaseDatabase.getInstance().reference.child("users")
 
-
         binding.loginButton.setOnClickListener {
             val username = binding.loginUsername.text.toString()
             val password = binding.loginPassword.text.toString()
             val currentUser = firebaseAuth.currentUser
-
             if (username.isNotEmpty() && password.isNotEmpty()) {
                 firebaseAuth.signInWithEmailAndPassword(username, password)
                     .addOnCompleteListener(this) { task ->
                         if (task.isSuccessful) {
                             // checks for email verification using firebase authentication
-                            if(currentUser!!.isEmailVerified) {
-                                Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
-                                val intent = Intent(this, MainActivity::class.java)
-                                startActivity(intent)
-                                finish()
+                            if (currentUser!!.isEmailVerified) {
+                                // Check if the user has a profile in the Realtime Database
+                                //checkUserProfile(currentUser.uid)
+                                startActivity(Intent(this@LoginActivity, CreateProfileActivity::class.java))
                             } else {
-                                // if email still not verified, meaning fresh account, redirect to VerifyEmailActivity
-                                Toast.makeText(this, "This email is not yet verified, please verify email!",Toast.LENGTH_SHORT).show()
-                                startActivity(Intent(this,LoginActivity::class.java))
-                                finish()
+                                // if email still not verified, meaning a fresh account, deny login entry and display toast
+                                Toast.makeText(this, "This email is not yet verified, please verify email!", Toast.LENGTH_SHORT).show()
                             }
                         } else {
                             Toast.makeText(this, "Login failed: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
@@ -60,5 +56,23 @@ class LoginActivity : AppCompatActivity() {
             startActivity(Intent(this@LoginActivity, RegistrationActivity::class.java))
             finish()
         }
+    }
+    private fun checkUserProfile(uid: String) {
+        databaseReference.child(uid).addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    // User has a profile, redirect to MainActivity
+                    startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+                } else {
+                    // User does not have a profile, redirect to CreateProfileActivity
+                    startActivity(Intent(this@LoginActivity, CreateProfileActivity::class.java))
+                }
+                finish()
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Handle error if needed
+            }
+        })
     }
 }
